a8ace88944f2295aa6e3eb8030cd976c9f60eab4
Jochem <git@jochem.tk>
2022-02-17 20:06:24 +0100

    Adding comments to sample files and working on tutorial

--- Teachable-machine-script.js
+++ Teachable-machine-script.js
@@ -1,19 +1,68 @@
+/*
+Note: I did not create all of this code myself. Most of this code is like it's
+provided by the export panel of the Teachable Machine and the code to show the
+oploaded image on the webpage is a combination from various tutorials on the
+internet. Finally, I received help from my uncle, who is an experienced webdeveloper
+with a lot of knowledge about JavaScript, to make it all work together.
+
+I am no webdeveloper, I had and still have no idea how to make something like this work.
+All I know is that this works and that it might be helpful to someone.
+*/
+
 // More API functions here:
         // https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/image
-
-        // the link to your model provided by Teachable Machine export panel
         
+        // Replace this link with the link to your model provided by Teachable Machine export panel
+        const URL = "https://teachablemachine.withgoogle.com/models/zZbsPBcuC/";
+
+        // These are some variables needed by the model
+        let model, webcam, labelContainer, maxPredictions, certainty, result;
+
+        // This function will initiate the model when the webpage is loaded, 
+        // so the user does not have to wait for it when using it.
         window.onload = (e) => {
             console.log('Model initiated');
             init();
         }
-
-        const URL = "https://teachablemachine.withgoogle.com/models/zZbsPBcuC/";
-
-        let model, webcam, labelContainer, maxPredictions, certainty, category;
         
+        // Load the image model
+        async function init() {
+            const modelURL = URL + "model.json";
+            const metadataURL = URL + "metadata.json";
+
+            // load the model and metadata
+            model = await tmImage.load(modelURL, metadataURL);
+            maxPredictions = model.getTotalClasses();
+            
+            labelContainer = document.getElementById("label-container");
+            for (let i = 0; i < maxPredictions; i++) { // and class labels
+                labelContainer.appendChild(document.createElement("div"));
+            }
+        }
         
-	    const status = document.getElementById('status');
+        // Run the image through the model, and show the result in the label container
+        async function predict() {
+            // predict can take in an image, video or canvas html element
+            const prediction = await model.predict(document.getElementById("output"));
+            console.log(prediction)
+            for (let i = 0; i < maxPredictions; i++) {
+                const classPrediction =
+                    prediction[i].className + ": " + prediction[i].probability.toFixed(2);
+                labelContainer.childNodes[i].innerHTML = classPrediction;
+                
+                // This code will save the result with a certainty of over 70%
+                // to a variable
+    			certainty = parseFloat(prediction[i].probability.toFixed(2));
+			    if (certainty > 0.70) {
+    				result = prediction[i].className;
+			    }
+            }
+            // window.location.assign(category + ".html");
+        }
+
+        // This code will show the uploaded image on the webpage
+        // and show an error if the file type is not supported.
+        const status = document.getElementById('status');
 	    const output = document.getElementById('output');
 	    if (window.FileList && window.File && window.FileReader) {
     		document.getElementById('file-selector').addEventListener('change', event => {
@@ -33,66 +82,13 @@
     				output.src = event.target.result;
 			    });
 			    reader.readAsDataURL(file);
-                //predict();
                 console.log('Image uploaded')
-                console.log(output)
             });
         }
         
-        
-        // Load the image model and setup the webcam
-        async function init() {
-            const modelURL = URL + "model.json";
-            const metadataURL = URL + "metadata.json";
-
-            // load the model and metadata
-            // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
-            // or files from your local hard drive
-            // Note: the pose library adds "tmImage" object to your window (window.tmImage)
-            model = await tmImage.load(modelURL, metadataURL);
-            maxPredictions = model.getTotalClasses();
-
-            // Convenience function to setup a webcam
-            // const flip = true; // whether to flip the webcam
-            // webcam = new tmImage.Webcam(200, 200, flip); // width, height, flip
-            // await webcam.setup(); // request access to the webcam
-            // await webcam.play();
-            // window.requestAnimationFrame(loop);
-
-            // append elements to the DOM
-            // document.getElementById("webcam-container").appendChild(webcam.canvas);
-            labelContainer = document.getElementById("label-container");
-            for (let i = 0; i < maxPredictions; i++) { // and class labels
-                labelContainer.appendChild(document.createElement("div"));
-            }
-        }
-        
-        // async function loop() {
-        //     webcam.update(); // update the webcam frame
-        //     await predict();
-        //     window.requestAnimationFrame(loop);
-        // }
-            
-        // run the webcam image through the image model
-        async function predict() {
-            // predict can take in an image, video or canvas html element
-            const prediction = await model.predict(document.getElementById("output"));
-            // console.log(document.getElementById("output"))
-            console.log(prediction)
-            for (let i = 0; i < maxPredictions; i++) {
-                const classPrediction =
-                    prediction[i].className + ": " + prediction[i].probability.toFixed(2);
-                labelContainer.childNodes[i].innerHTML = classPrediction;
-                
-    			certainty = parseFloat(prediction[i].probability.toFixed(2));
-			    if (certainty > 0.70) {
-    				category = prediction[i].className;
-			    }
-            }
-            // window.location.assign(category + ".html");
-        }
-        
+        // This simple function is called by the "next page" button in the HTML file.
+        // In this case, it simply takes the result from the model and opens the HTML file with that name.
 	    async function nextpage() {
             await predict();
-    		window.location.assign(category + ".html");
+    		window.location.assign(result + ".html");
     	}
--- docs/model.md
+++ docs/model.md
@@ -3,4 +3,6 @@ layout: page
 title: Exporting Model
 permalink: /model/
 ---
-Putting some text here, it might be needed or something.
+When making a model, you want to export it in the default mode. This means the model will be hosted by Google, all you need is the URL. You could copy the code provided by Google if you want to use it with a webcam, but if you want to use it with a file upload you can use my example code instead, which is explained on [this page](/coding).
+
+> If you want to use Teachable Machine to change the webpage a certain button link to, as is explained in this tutorial, you need to make sure that the classnames in the model you trained correspond with the names of the HTML files you want to open. For example, if the model has three classes ("red", "green" and "blue"), you need your HTML files to be named `red.html`, `green.html` and `blue.html`.
